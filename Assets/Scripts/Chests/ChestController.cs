db415517e36b089145b7b9437e28a37cbc5bd703
Chintan Patel <chintanpatelit024@gmail.com>
2025-07-16 11:47:46 +0530

    Added: ChestController

--- Assets/Scripts/Chests/ChestController.cs
+++ Assets/Scripts/Chests/ChestController.cs
@@ -1,3 +1,71 @@
-public class ChestController
+using ChestSystem.Utilities;
+using System;
+
+namespace ChestSystem.Chests
 {
+    public class ChestController
+    {
+        private ChestScriptableObject chestData;
+        private ChestState currentState;
+        private DateTime unlockStartTime;
+
+        public ChestController(ChestScriptableObject data)
+        {
+            chestData = data;
+            currentState = ChestState.LOCKED;
+        }
+
+        public void OnChestClicked()
+        {
+            switch (currentState)
+            {
+                case ChestState.LOCKED:
+                    StartUnlocking();
+                    break;
+                case ChestState.UNLOCKED:
+                    CollectChest();
+                    break;
+            }
+        }
+
+        public void StartUnlocking()
+        {
+            if (currentState != ChestState.LOCKED) return;
+
+            currentState = ChestState.UNLOCKING;
+            unlockStartTime = DateTime.UtcNow;
+        }
+
+        public void CollectChest()
+        {
+            if (currentState == ChestState.UNLOCKED)
+            {
+                currentState = ChestState.OPENED;
+                // TODO: Grant rewards
+            }
+        }
+
+        public void UpdateState()
+        {
+            if (currentState == ChestState.UNLOCKING)
+            {
+                TimeSpan elapsed = DateTime.UtcNow - unlockStartTime;
+                if (elapsed.TotalMinutes >= ConvertUnlockTimerinMinutes(chestData.unlockDurationMinutes))
+                    currentState = ChestState.UNLOCKED;
+            }
+        }
+
+        public string GetRemainingTimeFormatted()
+        {
+            TimeSpan remaining = TimeSpan.FromMinutes(ConvertUnlockTimerinMinutes(chestData.unlockDurationMinutes)) - (DateTime.UtcNow - unlockStartTime);
+            if (remaining.TotalSeconds < 0) return StringConstants.ZeroTimer;
+            return StringConstants.GetFormattedRemainingTime(remaining);
+        }
+
+        public ChestScriptableObject GetChestData() => chestData;
+
+        public ChestState GetChestState() => currentState;
+
+        private int ConvertUnlockTimerinMinutes(ChestUnlockTimer chestUnlockTimer) => (int)chestUnlockTimer;
+    }
 }
--- Assets/Scripts/Utilities/StringConstants.cs
+++ Assets/Scripts/Utilities/StringConstants.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace ChestSystem.Utilities
 {
     public static class StringConstants
@@ -6,5 +8,10 @@ namespace ChestSystem.Utilities
         public const string ChestUnlocking = "Unlocking in ";
         public const string ChestUnlocked = "Unlocked.";
         public const string ChestOpened = "Opened.";
+
+        public const string ZeroTimer = "00:00:00";
+
+        public static string GetFormattedRemainingTime(TimeSpan remaining) =>
+            string.Format("{0:D2}:{1:D2}:{2:D2}", remaining.Hours, remaining.Minutes, remaining.Seconds);   
     }
 }
